aeaf8ef5f9ab1469f4bf68e48e3412429e142c01
Nadya Khairunnisa <43774677+fatihsyams@users.noreply.github.com>
2022-11-05 11:17:24 +0700

    Update php

---
+++ PHP-DASAR/hello-world.php
@@ -0,0 +1,8 @@
+<!-- 'file php di akhiri dengan extention.php'
+di awal kode program phpwajib menambahkan "<?php //dan di akhir kode perlu di tambahkan ?> (tidak wajib)
+'Nama file PH tidak ada aturan, sama seperti membuat file lain biasanya
+Namun agar mempermudah menjalankan file php direkomendasikan tidak menggunakan spasi, jika ada spasi, nanti agak menyulitkan mengetikkannya di url, bisa berubah'
+'untuk menampilkan tulisan di php kita bisa menggunakan perintah echo'
+ -->
+<?php
+echo "Hello world";
